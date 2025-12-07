03dae861edbe75065550301f02f1f1b11f6c4838
fatima-saleem390 <fatimasalim319@gmail.com>
2025-12-07 18:09:38 +0500

    api/models/like.js

--- api/models/like.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const LikeSchema = new mongoose.Schema({
-  story: { type: mongoose.Schema.Types.ObjectId, ref: "Story", required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Like", LikeSchema);
