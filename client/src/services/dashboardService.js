5800d3f209d2f9b57a24b5c47d9fe2c905b2441f
fatima-saleem390 <fatimasalim319@gmail.com>
2025-12-07 18:27:15 +0500

    dashboardService.js

--- client/src/services/dashboardService.js
+++
@@ -1,7 +0,0 @@
-// [Frontend] client/src/services/dashboardService.js
-import axiosInstance from "./axiosInstance";
-
-export async function fetchDashboard() {
-  const res = await axiosInstance.get("/dashboard");
-  return res.data;
-}
