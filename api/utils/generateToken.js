cf2be965aba7482c4cb0fda9af9db87d51ae6e69
Sibgha N <sibghanadeem1312@gmail.com>
2025-12-09 08:24:12 +0500

    Update generateToken.js

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -1,7 +1,11 @@
+// jsonwebtoken library is for creating and verifying JWTs
 const jwt = require("jsonwebtoken");
+
+// this function to generate a JWT for a given user ID
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
-
+// sytax: jwt.sign(payload, secretOrPrivateKey, options)
+// Payload: contains the user ID in the token, Secret:  secret key stored in environment variables
 module.exports = generateToken;
