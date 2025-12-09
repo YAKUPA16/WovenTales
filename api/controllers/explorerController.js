c75779f8ded9663219e975d0d31f98c5547e2cdd
Sibgha N <sibghanadeem1312@gmail.com>
2025-12-09 09:11:39 +0500

    Create explorerController.js

---
+++ api/controllers/explorerController.js
@@ -0,0 +1,15 @@
+// [Backend] api/controllers/explorerController.js
+const Story = require("../models/Story");
+
+exports.getExploreStories = async (req, res) => {
+  try {
+    const stories = await Story.find({})
+      .sort({ createdAt: -1 })
+      .populate("author", "username name")
+      .lean({ virtuals: true }); 
+
+    return res.json({ stories });
+  } catch (e) {
+    res.status(500).json({ message: "Failed to load stories", error: e.message });
+  }
+};
